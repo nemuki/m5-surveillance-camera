ba1557c0d440e256c8dc57067ef0ade77242c303
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-11-29 12:32:29 +0900

    コードの更新

--- gas/code.js
+++ gas/code.js
@@ -1,19 +1,20 @@
 function myFunction() {
-  
+
 }
 
 function doPost(e) {
-  var data = Utilities.base64Decode(e.parameters.data);
-  var nombreArchivo = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'yyyyMMdd_HHmmss') + ".jpg";
-  var blob = Utilities.newBlob(data, e.parameters.mimetype, nombreArchivo);
+  const data = Utilities.base64Decode(e.parameters.data);
+  const nombreArchivo = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'yyyyMMdd_HHmmss') + ".jpg";
+  const blob = Utilities.newBlob(data, e.parameters.mimetype, nombreArchivo);
 
   // Save the photo to Google Drive
-  var folder, folders = DriveApp.getFoldersByName("ESP32");
+  const folders = DriveApp.getFoldersByName("ESP32");
+  let folder
   if (folders.hasNext()) {
     folder = folders.next();
   } else {
     folder = DriveApp.createFolder("ESP32");
   }
-  var file = folder.createFile(blob);
+  folder.createFile(blob);
   return ContentService.createTextOutput('Complete')
 }
