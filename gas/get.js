cf4b35acb4b740368ebea4401bf48cec08a551b4
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-12-20 10:45:47 +0900

    GASからコードをpullした

--- gas/get.js
+++ gas/get.js
@@ -1,3 +1,23 @@
-function doGet() {
-  return HtmlService.createTemplateFromFile('index.html').evaluate();
+function getData() {
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const sheet = ss.getActiveSheet()
+  const rows = sheet.getDataRange().getValues();
+  const keys = rows.splice(0, 1)[0];
+  return rows.map(function (row) {
+    let obj = {}
+    row.map((item, index) => {
+      obj[keys[index]] = item;
+    });
+    return obj;
+  });
+}
+
+function doGet(e) {
+  if (e.parameters.date) {
+    const data = getData();
+    return ContentService.createTextOutput(JSON.stringify(data, null, 2))
+      .setMimeType(ContentService.MimeType.JAVASCRIPT);
+  } else {
+    return HtmlService.createHtmlOutputFromFile('index');
+  }
 }
--- gas/post.js
+++ gas/post.js
@@ -1,16 +1,20 @@
 function myFunction() {
   const today = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'YYYY-MM-dd')
 
-  // Save the photo to Google Drive
-  let rootFolder = DriveApp.getFolderById(env())
-  let targetFolder = rootFolder.getFoldersByName(today)
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const sheet = ss.getActiveSheet()
 
-  while (targetFolder.hasNext()) {
-    let file = targetFolder.next();
-    
-    let fileName = file.getName();
-    console.log(fileName);
+  // Save the photo to Google Drive
+  let saveFolder, rootFolder = DriveApp.getFolderById(env())
+  let folders = rootFolder.getFolders();
+  for (key in folders) {
+    Logger.log()
   }
+  if (rootFolder.getFoldersByName(today).hasNext())
+    saveFolder = rootFolder.getFoldersByName(today).next()
+  else
+    saveFolder = rootFolder.createFolder(today)
+  sheet.appendRow([new Date(), saveFolder.getId()])
 }
 
 function doPost(e) {
@@ -18,15 +22,20 @@ function doPost(e) {
   const imageData = Utilities.base64Decode(e.parameters.data);
   const imageName = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'YYYY/MM/dd hh-mm-ss') + ".jpg";
   const blob = Utilities.newBlob(imageData, e.parameters.mimetype, imageName);
-  const today = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'YYYY/MM/dd')
+
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const sheet = ss.getActiveSheet()
 
   // Save the photo to Google Drive
+  const today = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'YYYY/MM/dd')
   let saveFolder, rootFolder = DriveApp.getFolderById(env())
   if (rootFolder.getFoldersByName(today).hasNext())
     saveFolder = rootFolder.getFoldersByName(today).next()
   else
     saveFolder = rootFolder.createFolder(today)
-  saveFolder.createFile(blob)
+  let saveFile = saveFolder.createFile(blob)
+
+  sheet.appendRow([new Date(), saveFolder.getId(), saveFile.getUrl()])
 
   return ContentService.createTextOutput('Complete')
 }
